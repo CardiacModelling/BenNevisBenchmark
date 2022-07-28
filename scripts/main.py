a6218f614f79b174cd0eefddc23c7f016f3e82c2
ericway1024 <ericway1024@gmail.com>
2022-07-28 17:26:21 +0100

    Allow downsampling

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,13 @@
 import nevis
-from config import GRID_SIDE
+from config import GRID_SIDE, GRID_DOWNSAMPLING
 from run_grid_search import run_grid_search
 from plot_random_method import plot_random_method, read_results
 import matplotlib.pyplot as plt
 
+
 run_grid_search()
-points_list, function_values, distance_values = read_results(f'grid_search_{GRID_SIDE}_')
+points_list, function_values, distance_values, downsampling = read_results(f'grid_search_{GRID_SIDE}_{GRID_DOWNSAMPLING}_')
 t = nevis.Timer()
-plot_random_method(points_list, function_values, distance_values)
+plot_random_method(points_list, function_values, distance_values, method_name='Grid Search', downsampling=downsampling)
 print(t.format())
 plt.show()
