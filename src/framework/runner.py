e36d6d023b0d80fc0e3917e2628a1007564b5c14
ericway1024 <ericway1024@gmail.com>
2022-08-31 17:15:58 +0100

    Add support for grad

--- src/framework/runner.py
+++ src/framework/runner.py
@@ -5,6 +5,7 @@ from functools import wraps
 from .result import Result
 
 f = nevis.linear_interpolant()
+f_grad = nevis.linear_interpolant(grad=True)
 
 def optimizer(opt):
     
@@ -12,10 +13,15 @@ def optimizer(opt):
     def func(**params):
         points = []
         function_values = []
-        def wrapper(u):
+        def wrapper(u, grad=None):
             x, y = u
             points.append((x, y))
-            z = f(x, y)
+            if grad is not None and grad.size > 0:
+                z, (gx, gy) = f_grad(x, y)
+                grad[0] = gx
+                grad[1] = gy
+            else:
+                z = f(x, y)
             function_values.append(z)
             return -z
         
