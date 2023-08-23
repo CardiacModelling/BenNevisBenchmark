08fd7d75768e78c8f7bd83706604c42a2e715951
ericway1024 <ericway1024@gmail.com>
2023-08-23 12:53:50 +0100

    Minor changes

--- src/algorithms/mlsl.py
+++ src/algorithms/mlsl.py
@@ -6,18 +6,19 @@ import numpy as np
 @optimizer
 def run_mlsl(f, x_max, y_max, population):
     opt = nlopt.opt(nlopt.G_MLSL, 2)
+    nlopt.srand(seed=10)
     opt.set_lower_bounds([0, 0])
     opt.set_upper_bounds([x_max, y_max])
     opt.set_min_objective(f)
 
     local_opt = nlopt.opt(nlopt.LN_NELDERMEAD, 2)
-    local_opt.set_ftol_abs(1e-1)
-    local_opt.set_xtol_abs(1e-1)
+    local_opt.set_ftol_abs(0.2)
+    local_opt.set_xtol_abs(10)
 
     opt.set_local_optimizer(local_opt)
     opt.set_population(int(population))
-    opt.set_ftol_abs(1e-1)
-    opt.set_xtol_abs(1e-1)
+    opt.set_ftol_abs(0.2)
+    opt.set_xtol_abs(10)
     opt.set_maxeval(MAX_FES)
 
     x, y = opt.optimize([1, 1])
