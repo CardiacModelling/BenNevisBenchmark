2cca4024bf2505102ea0c13e0cada8490e0441a9
Yuhang Eric Wei <git@yuha.ng>
2023-09-03 20:23:16 +0100

    Add cmaes

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -3,7 +3,7 @@ from .dual_annealing import dual_annealing
 from .mlsl import mlsl
 # from .multistart import multistart
 # from .simulated_annealing import simulated_annealing
-# from .cmaes import cmaes
+from .cmaes import cmaes
 
 __all__ = [
     # 'shgo', 
@@ -11,5 +11,5 @@ __all__ = [
     'mlsl', 
     # 'multistart',
     # 'simulated_annealing', 
-    # 'cmaes'
+    'cmaes',
 ]
