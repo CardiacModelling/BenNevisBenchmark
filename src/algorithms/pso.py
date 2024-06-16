b91e3fb890d835f3304c35a3ea0c0861f0ac09a9
Yuhang Eric Wei <git@yuha.ng>
2024-06-16 22:47:48 +0100

    update algorithms

--- src/algorithms/pso.py
+++ src/algorithms/pso.py
@@ -36,24 +36,28 @@ def run_pso(
     sigma0 = trial.suggest_float('sigma0', width / 20, width / 2)
     r = trial.suggest_float('r', 0, 1)
     population_size = trial.suggest_int('population_size', 1, 1000)
+
     e = Error(f)
-    opt = pints.OptimisationController(
-        e,
-        x0=init_guess,
-        sigma0=sigma0,
-        boundaries=b,
-        method=pints.PSO,
-    )
-
-    opt.optimiser().set_population_size(population_size)
-    opt.optimiser().set_local_global_balance(r)
-    opt.set_max_evaluations(get_budget())
-    opt.set_threshold(-SUCCESS_HEIGHT)
-    opt.set_max_unchanged_iterations(10, FTOL)
-    opt.set_log_to_screen(False)
-
-    x, z = opt.run()
-
+    x, z = None, float('inf')
+    i = 0
+    while get_budget() > 0 and z > -SUCCESS_HEIGHT:
+        opt = pints.OptimisationController(
+            e,
+            x0=b.sample() if i > 0 else init_guess,
+            sigma0=sigma0,
+            boundaries=b,
+            method=pints.PSO,
+        )
+        opt.optimiser().set_population_size(population_size)
+        opt.optimiser().set_local_global_balance(r)
+        opt.set_max_evaluations(get_budget())
+        opt.set_threshold(-SUCCESS_HEIGHT)
+        opt.set_max_unchanged_iterations(10, FTOL)
+        opt.set_log_to_screen(False)
+        x1, z1 = opt.run()
+        if z1 < z:
+            x, z = x1, z1
+        i += 1
     return {
         'x': x,
         'z': z,
@@ -61,7 +65,12 @@ def run_pso(
 
 
 pso = Algorithm(
-    'pso',
-    run_pso,
-    1,
+    name='pso',
+    func=run_pso,
+    version=3,
+    default_params={
+        'r': 0.5,
+        'sigma0': 1.2e5,
+        'population_size': 6,
+    }
 )
