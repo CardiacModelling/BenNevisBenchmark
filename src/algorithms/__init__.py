ebf343aebaee12e33fdb739dac17813658328b7e
Yuhang Eric Wei <git@yuha.ng>
2024-06-19 11:14:31 +0100

    add differential evolution

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -5,7 +5,7 @@ from .ipop_cmaes import ipop_cmaes
 from .pso import pso
 from .nelder_mead import nelder_mead
 from .nelder_mead_multi import nelder_mead_multi
-
+from .differential_evolution import differential_evolution
 __all__ = [
     'dual_annealing',
     'mlsl',
@@ -13,4 +13,5 @@ __all__ = [
     'pso',
     'nelder_mead',
     'nelder_mead_multi',
+    'differential_evolution',
 ]
---
+++ src/algorithms/differential_evolution.py
@@ -0,0 +1,68 @@
+from framework import optimizer, Algorithm, SUCCESS_HEIGHT, FTOL
+import scipy.optimize
+import optuna
+
+
+@optimizer
+def run_differential_evolution(
+    f,
+    x_max,
+    y_max,
+    rand_seed,
+    init_guess,
+    trial: optuna.Trial,
+    get_budget,
+):
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.differential_evolution.html
+    def callback(intermediate_result):
+        # Define your custom stopping condition here
+        # For example, stop when the function value is below a threshold
+        if intermediate_result.fun < -SUCCESS_HEIGHT:
+            return True
+
+        if get_budget() <= 0:
+            return True
+
+    popsize = trial.suggest_int('popsize', 10, 50)
+    recombination = trial.suggest_float('recombination', 0, 1)
+    polish = trial.suggest_categorical('polish', [True, False])
+    dithering = trial.suggest_categorical('dithering', [True, False])
+    mutation_low = trial.suggest_float('mutation_low', 0, 2)
+    mutation_high = trial.suggest_float('mutation_high', mutation_low, 2)
+    mutation = mutation_low if not dithering else (mutation_low, mutation_high)
+
+    ret = scipy.optimize.differential_evolution(
+        f,
+        bounds=[(0, x_max), (0, y_max)],
+        strategy='best1bin',
+        maxiter=2500,
+        popsize=popsize,
+        tol=0,
+        atol=FTOL,
+        mutation=mutation,
+        recombination=recombination,
+        seed=rand_seed,
+        callback=callback,
+        polish=polish,
+        x0=init_guess,
+        init='latinhypercube',
+    )
+    return {
+        'x': ret.x,
+        'z': ret.fun,
+    }
+
+
+differential_evolution = Algorithm(
+    name='Differential Evolution',
+    func=run_differential_evolution,
+    version=1,
+    default_params={
+        'popsize': 15,
+        'recombination': 0.7,
+        'polish': False,
+        'dithering': True,
+        'mutation_low': 0.5,
+        'mutation_high': 1,
+    }
+)
